7a44f8cfa524fdc9a25da10d62f4402f03bb757a
CAustin95 <courtneyaustin@utexas.edu>
2018-11-30 00:09:29 -0600

    Add files via upload

---
+++ mccombsdataentry.sql
@@ -0,0 +1,163 @@
+`mccombs`.`Articles` SET `Title` = <{Title: }>, `PrimaryAuthor` = <{PrimaryAuthor: }>, `PrimaryInstitution` = <{PrimaryInstitution: }>, `PrimaryDepartment` = <{PrimaryDepartment: }>, `SecondaryAuthor` = <{SecondaryAuthor: }>, `SecondaryInstitution` = <{SecondaryInstitution: }>, `SecondaryDepartment` = <{SecondaryDepartment: }>, `TertiaryAuthor` = <{TertiaryAuthor: }>, `TertiaryInstitution` = <{TertiaryInstitution: }>, `TertiaryDepartment` = <{TertiaryDepartment: }>, `Topics` = <{Topics: }> WHERE
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW DATABASES
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW TABLES FROM `mccombs` like 'mccombs 2011'
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW DATABASES
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`id`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)'
+DEALLOCATE PREPARE stmt
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW DATABASES
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`id`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)'
+DEALLOCATE PREPARE stmt
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW DATABASES
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`id`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)'
+DEALLOCATE PREPARE stmt
+Refresh Recordset
+
+Refresh Recordset
+
+Refresh Recordset
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`id`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+Refresh Recordset
+
+Refresh Recordset
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+Refresh Recordset
+
+SELECT * FROM mccombs.articles LIMIT 0, 10000
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+DEALLOCATE PREPARE stmt
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW COLUMNS FROM `McCombs`.`articles`
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+SHOW COLUMNS FROM `McCombs`.`articles`
+
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW DATABASES
+
+SHOW SESSION VARIABLES LIKE 'lower_case_table_names'
+
+SHOW COLUMNS FROM `McCombs`.`articles`
+PREPARE stmt FROM 'INSERT INTO `McCombs`.`articles` (`Title`,`Topics`,`TertiaryInstitution`,`SecondaryInstitution`,`PrimaryInstitution`,`PrimaryDepartment`,`Year`,`SecondaryAuthor`,`TertiaryAuthor`,`TertiaryDepartment`,`SecondaryDepartment`,`PrimaryAuthor`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+
+DEALLOCATE PREPARE stmt
+
+SELECT * FROM mccombs.articles LIMIT 0, 10000
+
+SELECT * FROM mccombs.articles LIMIT 0, 10000
+
+SELECT * FROM mccombs.articles LIMIT 0, 10000
+
+SELECT * FROM mccombs.articles LIMIT 0, 10000
